93311b2505c05d3d1ed48cc65c53aaff810c0d4c
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-09 12:40:59 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,24 +1,24 @@
-import sys 
-
+import sys
 def add(num1 , num2):
     add = (num1 + num2)
     return add
-def sub(num1 , num2):   
-    sub(num1 - num2)
+def sub(num1 , num2):
+    sub = (num1 - num2)
     return sub
+
 def mul(num1 , num2):
     mul = (num1 * num2)
     return mul
 def div(num1 , num2):
     div = (num1 / num2)
-    return div    
+    return div
 num1 = float(sys.argv[1])
 operation = sys.argv[2]
 num2 = float(sys.argv[3])
 
 if operation == "add":
-    output =(num1 + num2)
-    print(output)
+    output = (num1 + num2)
+    print (output)
 
 if operation == "sub":
     output =(num1 - num2)
