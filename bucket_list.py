b764e51cd005b03ef7b6f59b003726c41026ef5d
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-11 14:09:24 +0300

    Update bucket_list.py

--- bucket_list.py
+++ bucket_list.py
@@ -1,9 +1,18 @@
 # lists
 
 s3_buckets_lists =  ["sharfuddin_bucket",  "sharfuddin_siaf_bucket",  "sanaf_bucket"]
-print(s3_buckets_lists[2])
+print(len(s3_buckets_lists))
 
+s3_buckets_lists.append("new_s3_bucket")
+print(len(s3_buckets_lists))
+
+
+s3_buckets_lists.remove("sanaf_bucket")
+print(len(s3_buckets_lists))
 
 
 
 argument[2]
+print(len(s3_buckets_lists)) #### for bucket length
+s3_buckets_lists =  ["sharfuddin_bucket",  "sharfuddin_siaf_bucket",  "sanaf_bucket"]
+print(s3_buckets_lists[2]
