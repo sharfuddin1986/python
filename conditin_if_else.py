d747d3ff00063645fa62f6fd2f3cae8f1dc3feac
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-11 11:43:22 +0300

    Create conditin_if_else.py

---
+++ conditin_if_else.py
@@ -0,0 +1,9 @@
+import sys
+type = sys.argv[1]
+if type == "t2.micro":
+    print("ok it will create soon")
+elif type == "x2.large":
+    print("this is charge 8$ per hour")
+
+else: 
+    print("this is not in our scope")
