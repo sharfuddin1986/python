f29ec6050c48acf30f3ac0779c7213d24be18ae8
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-16 08:43:26 +0300

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,18 @@
+# Task 1: Arithmetic Operators
+
+Create two variables a and b with numeric values.
+Calculate the sum, difference, product, and quotient of a and b.
+Print the results.
+
+a = 10
+b = 5
+sum_result = a + b
+
+difference_result = a - b
+product_result = a * b
+quotient_result = a / b
+print("sum:", sum_result)
+print("difference:", difference_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
+
