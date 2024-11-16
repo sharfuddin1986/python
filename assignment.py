b08f5f9eb6086c9f1f73a6991f62da449d4d6d93
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-16 09:13:29 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -19,3 +19,25 @@ print("Quotient:", quotient_result)
 # Task 2: Comparison Operators
 Compare the values of a and b using the following comparison operators: <, >, <=, >=, ==, and !=.
 Print the results of each comparison.
+
+a = 10
+b = 5
+
+
+less_than = a < b
+greater_than = a > b
+less_than_or_equal = a <= b
+greater_than_or_equal = a >= b
+equal = a == b
+not_equal = a != b
+print("a < b:", less_than)
+print("a > b:", greater_than)
+print("a <= b:", less_than_or_equal)
+print("a >=:", greater_than_or_equal)
+print("a == b :", equal)
+print("a != b:", not_equal)
+
+## Task 3: Logical Operators
+Create two boolean variables, x and y.
+Use logical operators (and, or, not) to perform various logical operations on x and y.
+Print the results.
