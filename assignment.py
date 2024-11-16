579919c287b64b3f5a8337d78fefb161429c7dfa
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-16 10:34:24 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -63,4 +63,11 @@ Use assignment operators (+=, -=, *=, /=) to update the value of total.
 Print the final value of total.
 
 
-  
+total = 10
+
+total += 5
+total -= 3
+total *= 2
+total /= 4
+
+print("Final total:", total)
