0894d5abb43ae6b92755d2c0b7ab7bbb2705087e
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-16 09:25:26 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -42,3 +42,25 @@ print("a != b:", not_equal)
 Create two boolean variables, x and y.
 Use logical operators (and, or, not) to perform various logical operations on x and y.
 Print the results.
+
+
+x = True
+y = False
+
+and_result = x and y
+or_result = x or y 
+not_result_x = not x
+not_result_y = not y
+
+print("x and y:", and_result)
+print("x or y:", or_result)
+print("not x:", not_result_x)
+print("not y:", not_result_y)
+
+# Task 4: Assignment Operators
+Create a variable total and initialize it to 10.
+Use assignment operators (+=, -=, *=, /=) to update the value of total.
+Print the final value of total.
+
+
+  
