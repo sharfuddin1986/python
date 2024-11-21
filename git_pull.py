0715872c3350b5368d6b1b95728b1ef9a8190e44
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-21 08:03:53 +0300

    Update git_pull.py

--- git_pull.py
+++ git_pull.py
@@ -3,3 +3,15 @@ response = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pull
 complete_detail = response.json()
 for i in range (len(complete_detail)):
     print(complete_detail[i]["user"]["login"])
+
+
+
+
+### Get pull request information on reporting in python 
+kubernets-user_name
+kubernetes-repo_name
+
+1-first insatll request pip install request
+2- go to Google type github api after copy path pull request
+3-dictonary
+4- print 
