d8ea36db406e7c8e3734d094a7e27a37bc9e3eb9
Md Sharfuddin <106474548+sharfuddin1986@users.noreply.github.com>
2024-11-21 08:13:51 +0300

    Update instance_list.py

--- instance_list.py
+++ instance_list.py
@@ -1,26 +1,13 @@
-ec2_instance_info = [
-    {
-        "id": "instance001",
-        "type": "t2.micro",
-        "service": "Application_server_1"
-    },    
-    {   
-         "id": "instance002",
-        "type": "t2.medium",
-        "service": "web_server_1"
+server_config = {
+    'server1': {'ip': '192.168.1.1', 'port': 8080, 'status': 'active'},
+    'server2': {'ip': '192.168.1.2', 'port': 8000, 'status': 'inactive'},
+    'server3': {'ip': '192.168.1.3', 'port': 9000, 'status': 'active'}
+}
+def get_server_status(server_name):
+    return server_config.get(server_name, {}).get('status', 'Server not found')
+server_name = 'server1'
+status = get_server_status(server_name)
+print(f"{server_name} status: {status}")
 
-    },
-    { 
-        "id": "instance003",
-        "type": "t2.large",
-        "service": "Application_server_2"
-    },
-    {
-        "id": "instance004",
-        "type": "t2.medium",
-        "service": "web_server_2"
 
-    }
-
-]
-print(ec2_instance_info[0]["type"])
+## output server1 status: active
